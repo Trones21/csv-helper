031cf2aa0506267100d26223c7ceef793e51da19
Thomas Rones <trones.de@gmail.com>
2020-11-11 00:49:48 +0100

    Website MVP finished

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-
+console.log("Starting Server")
 app.use(express.static('public'))
 app.get('/', function (req, res) {
   res.send();
--- Website/public/worker.js
+++ Website/public/worker.js
@@ -1,91 +1,16 @@
-//Event Listeners at bottom of file
-
-function convert(){
-    const myWorker = new Worker("worker.js");
-    let fileNode = document.querySelector('.fileInput');
-    myWorker.postMessage({fileList:fileNode.files, delimiter: document.querySelector('#specifyDelimiter').value.trim() || ","});
-    myWorker.onmessage = function(e){
-        switch(e.data.msgType){
-            case "stepList":
-                updateStepList(e.data.stepClassName)
-                break;
-            case "progBar":
-                updateProgressBar(e.data.params.msg, e.data.params.pctDone)
-                break;
-            case "outputData":
-                let outfileName = document.querySelector('.fileInput').value.split('\\')[2].split('.')[0]
-                updateStepList('convertFileStatus');
-                updateProgressBar(`Converted! Writing CSV` , 95)
-                writeCSV(e.data.headersStr + e.data.recordsStr, outfileName);
-                updateProgressBar(`Finished!` , 100)
-                break;
-            case "error":
-                let errorDiv = document.createElement('div');
-                errorDiv.innerText = e.data.err;
-                document.getElementsByClassName('modal-body')[0].appendChild(errorDiv);
-                break;
-            default:
-                console.log("Invalid Case sent from Worker")
-                break;
-        }
-    }
-}
-
-const updateProgressBar = (Message, pctDone)=>{
-    document.querySelector('.progress-bar').setAttribute('aria-valuenow', pctDone);
-    document.querySelector('.progress-bar').setAttribute('style', 'width: ' + pctDone + '%');
-    document.querySelector('.progress-bar').children[0].innerHTML = Message
-} 
-
-
-const updateStepList = (stepClassName) => {
-    let stepList = document.querySelector('.stepList');
-    stepList.getElementsByClassName(stepClassName)[0].innerText = '\u2714';
-}
-
-const cleanUpModal = () => {
-    let modalBody = document.getElementsByClassName('modal-body')[0];
-    modalBody.innerHTML = `<table class="stepList">
-    <tr class="step">
-        <td class="loadFileStatus"></td>
-        <td>Load File</td>
-    </tr>
-    <tr class="step">
-        <td class="parseFileStatus"></td>
-        <td>Parse File</td>
-    </tr>
-    <tr class="step">
-        <td class="flattenArrayStatus"></td>
-        <td>Flatten Object Array</td>
-    </tr>
-    <tr class="step">
-        <td class="convertFileStatus"></td>
-        <td>Convert</td>
-    </tr>
-</table>
-<div class="progress">
-    <div class="progress-bar progress-bar-striped active" role="progressbar"
-        aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
-        <span></span>
-    </div>
-</div>`
-}
-
-
-const handleFilewithoutWorker = () => {
-    let fileNode = document.querySelector('.fileInput');
-    const fileList = fileNode.files;
+ onmessage = function(e){
+    let delimiter = e.data.delimiter
     let reader = new FileReader();
-    reader.onload = function(e) {
-        updateProgressBar("File Loaded", 0);
-        try{
-        let outfileName = document.querySelector('.fileInput').value.split('\\')[2].split('.')[0]
-        let data = JSON.parse(e.target.result)
-        updateProgressBar("File Parsed", 0)
-        let initialArray = data.map((i)=>JSON.flatten(i))
-        updateProgressBar("Array Flattened", 0);
-        let delimiter = document.querySelector('#specifyDelimiter').value.trim() || ",";
+    reader.onload = function(f) {
+        postMessage({msgType:"stepList", stepClassName:"loadFileStatus"});
         
+        try{   
+        let data = JSON.parse(f.target.result)
+        postMessage({msgType:"stepList", stepClassName:"parseFileStatus"});
+        
+        let initialArray = data.map((i)=>JSON.flatten(i))
+        postMessage({msgType:"stepList", stepClassName:"flattenArrayStatus"});
+    
         //Prep - I have non-uniform objects so I need to create a masterObject to ensure my columns line up 
         let masterObj = determineMasterObject(initialArray)
         let headersStr = getCSVstring_PropNamesAsHeaders(initialArray, masterObj, delimiter);
@@ -93,17 +18,14 @@ const handleFilewithoutWorker = () => {
         
         //Convert & Write
         let recordsStr = ConvertToCSV(cleanData, masterObj, delimiter);
-        updateProgressBar(`Finished! Writing File` , 100)
-        writeCSV(headersStr + recordsStr, outfileName);
-        
+        postMessage({msgType:"outputData", headersStr:headersStr, recordsStr:recordsStr})
         } catch(err){
-            console.log(err)
+            postMessage({msgType:"error", err:err})
         }
 
     }
 
-    reader.readAsText(fileList[0])
-
+    reader.readAsText(e.data.fileList[0]);
 }
 
 //CSV Functions
@@ -119,7 +41,7 @@ function ConvertToCSV(objArray, masterObj, delimiter) {
     for (var i = 0; i < array.length; i++) {
         if(((i/array.length)*100)%10 === 0){
             let pctDone = ((i/array.length)*100)
-            updateProgressBar(`Converting: ${pctDone}% Done` , pctDone)
+            postMessage({msgType:"progBar", params:{msg:"Converting: " + pctDone + "%" ,pctDone:pctDone}})
 
         }
         //Compare Properties 
@@ -200,20 +122,6 @@ function determineMasterObject(objArray) {
     }
 }
 
-
-function writeCSV(cleanData, fileNameNoExt) {
-
-    let data = new Blob([cleanData],{
-        type: "text/csv;charset=utf-8"
-    });
-    let url = window.URL.createObjectURL(data);
-
-    let dLink = document.createElement("a");
-    dLink.setAttribute("href", url);
-    dLink.download = fileNameNoExt + ".csv";
-    dLink.click();
-}
-
 function getCSVstring_PropNamesAsHeaders(objArray, masterObj, delimiter) {
     let headersObj = {};
     headersObj.str = "";
@@ -272,11 +180,3 @@ JSON.flatten = function(data) {
     recurse(data, "");
     return result;
 }
-
-let uploadBtn = document.getElementsByClassName('uploadbtn')[0];
-uploadBtn.addEventListener('click', convert, false);
-
-let modalCloseBtn = document.getElementsByClassName('modalClose')[0];
-modalCloseBtn.addEventListener('click', cleanUpModal, false);
-
-
